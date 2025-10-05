ab67be2f6ff3d9e8236dce9ed5f4e85f02a7659a
ABDUL RAFAY <rafay16031@gmail.com>
2025-10-05 17:57:14 +0500

    Set the Home Page

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Navbar from '@/Component/Nav'
 
 const page = () => {
   return (
-    <div className='text-white'>
-      rafay
+    <div className='text-white bg-[#25252b]'>
+      <Navbar />
     </div>
   )
 }
